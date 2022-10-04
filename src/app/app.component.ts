6c3af49d6d05c333272ebb72a27f54ea4820c685
kingwangwong <kingngngco@gmail.com>
2022-10-04 11:37:15 -0500

    Starting form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,13 @@ import { Component } from '@angular/core';
   template: `
   <nav class='navbar navbar-expand navbar-light bg-light'>
     <a class='navbar-brand'>{{pageTitle}}</a>
-    <ul class='nav nav-pills'>
-      <li><a class='nav-link' routerLink='/welcome'>Home</a></li>
-      <li><a class='nav-link' routerLink="/managers">Managers List</a></li>
+    <ul class='navbar-nav'>
+      <li class='nav-item'><a class='nav-link' routerLinkActive='active' 
+      [routerLink]="['/welcome']">Home</a></li>
+      <li class='nav-item'><a class='nav-link' routerLinkActive='active' 
+      [routerLink]="['/managers']">Managers List</a></li>
+      <li class='nav-item'><a class='nav-link' routerLinkActive='active' 
+      [routerLink]="['/managers/0/edit']">Add a Manager Review</a></li>
     </ul>
 </nav>
 <div class='container'>
@@ -18,5 +22,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  pageTitle = 'Angular: Getting Started';
+  pageTitle = 'RateMyManager';
 }
--- src/app/managers/manager.module.ts
+++ src/app/managers/manager.module.ts
@@ -1,22 +1,33 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { ManagerDetailComponent } from './manager-detail.component';
 import { ManagerDetailGuard } from './manager-detail.guard';
+import { ManagerEditComponent } from './manager-edit.component';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 import { ManagersComponent } from './managers.component';
 
 @NgModule({
     declarations: [
         ManagersComponent,
-        ManagerDetailComponent
+        ManagerDetailComponent,
+        ManagerEditComponent
     ],
     imports: [
+        InMemoryWebApiModule.forRoot(ManagerData),
+        ReactiveFormsModule,
         RouterModule.forChild([
             { path: 'managers', component: ManagersComponent },
             { 
               path: 'managers/:id', 
               canActivate: [ManagerDetailGuard],
               component: ManagerDetailComponent 
+            },
+            {
+                path: 'managers/:id/edit',
+                component: ManagerEditComponent
             }
           ]),
         SharedModule
